852c901be19aada77adf9799805d8abfcda09d47
BAAZIZ KHADIDJA <baazize-khadija@hotmail.com>
2021-03-22 10:25:23 +0100

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { ProfileComponent } from './components/profile/profile.component';
+import { HomeComponent } from './components/home/home.component';
+
+const routes: Routes = [
+  {path: '', component:HomeComponent },
+  {path: 'profile', component: ProfileComponent},];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
